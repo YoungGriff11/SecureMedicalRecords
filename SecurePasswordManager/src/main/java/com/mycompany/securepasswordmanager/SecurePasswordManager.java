f73ca98fa81eaca8bc6b203a3cc22b5c0aedf1f2
Liam Connell <116265975+liamo132@users.noreply.github.com>
2024-11-08 18:50:32 +0000

    GUI created for my Secure Passsword Manager.

---
+++ SecurePasswordManager/src/main/java/com/mycompany/securepasswordmanager/SecurePasswordManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.securepasswordmanager;
+
+/**
+ *
+ * @author liamf
+ */
+public class SecurePasswordManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
